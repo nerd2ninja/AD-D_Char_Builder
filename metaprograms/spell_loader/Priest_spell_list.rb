6dd7969f114af89c39bd34a2469f7ec2a8737e7b
nerd2ninja <23155951+nerd2ninja@users.noreply.github.com>
2024-09-23 18:49:53 -0400

    Create Priest_spell_list.rb

---
+++ metaprograms/spell_loader/Priest_spell_list.rb
@@ -0,0 +1,188 @@
+Spell Lists (Priest)
+
+ 
+
+1st Level
+
+Animal Friendship
+Bless
+
+Combine
+
+Command
+
+Create Water
+
+Cure Light Wounds
+Detect Evil
+
+Detect Magic
+
+Detect Poison
+
+10 Detect Snares & Pits
+11 Endure Heat/Endure Cold
+12 Entangle
+
+13. Faerie Fire
+
+14 Invisibility to Animals
+15 Invisibility to Undead
+16 Light
+
+17 Locate Animals or Plants
+18 Magical Stone
+
+19 Pass Without Trace
+20 Protection From Evil
+
+OCeBNANeone
+
+Priest Spells
+
+2nd Level
+
+Aid
+
+Augury
+
+Barkskin
+
+Chant
+
+Charm Person or Mammal
+Detect Charm
+Dust Devil
+Enthrall
+
+Find Traps
+
+Fire Trap
+
+Flame Blade
+Goodberry
+
+Heat Metal
+
+Hold Person
+Know Alignment
+Messenger
+Obscurement
+Produce Flame
+Resist Fire/Resist Cold
+Silence, 15’ Radius
+
+3rd Level
+
+Animate Dead
+
+Call Lightning
+
+Continual Light
+
+Create Food & Water
+Cure Blindness or Deafness
+Cure Disease
+
+Dispel Magic
+
+Feign Death
+
+Flame Walk
+
+Glyph of Warding
+
+Hold Animal
+
+Locate Object
+
+Magical Vestment
+
+Meld Into Stone
+
+Negative Plane Protection
+Plant Growth
+
+Prayer
+
+Protection From Fire
+Pyrotechnics
+
+Remove Curse
+
+4th Level
+
+Abjure
+
+Animal Summoning I
+
+Call Woodland Beings
+
+Cloak of Bravery
+
+Control Temperature, 10’ Radius
+Cure Serious Wounds
+
+Detect Lie
+
+Divination
+
+Free Action
+
+Giant Insect
+
+Hallucinatory Forest
+
+Hold Plant
+
+Imbue With Spell Ability
+
+Lower Water
+
+Neutralize Poison
+
+Plant Door
+
+Produce Fire
+
+Protection From Evil, 10' Radius
+Protection From Lightning
+Reflecting Pool
+
+ 
+
+21 Purify Food & Drink Slow Poison Remove Paralysis Repel Insects
+22 Remove Fear Snake Charm Snare Speak With Plants
+23 Sanctuary Speak With Animals Speak With Dead Spell Immunity
+24 Shillelagh Spiritual Hammer Spike Growth Sticks to Snakes
+25 Trip Starshine Tongues
+26 Warp Wood Stone Shape
+27 Withdraw Summon Insects
+28 Wyvern Watch Tree
+29 Water Breathing
+30 Water Walk
+Sth Level 6th Level 7th Level
+1 Air Walk Aerial Servant Animate Rock
+2 Animal Growth Animal Summoning III Astral Spell
+3 Animal Summoning II Animate Object Changestaff
+4 Anti-Plant Shell Anti-Animal Shell Chariot of Sustarre
+5 Atonement Blade Barrier Confusion
+6 Commune Conjure Animals Conjure Earth Elemental
+7 Commune With Nature Conjure Fire Elemental Control Weather
+8 Control Winds Find the Path Creeping Doom
+9 Cure Critical Wounds Fire Seeds Earthquake
+10 Dispel Evil Forbiddance Exaction
+11 Flame Strike Heal Fire Storm
+12 Insect Plague Heroes’ Feast Gate
+13 Magic Font Liveoak Holy Word
+14 Moonbeam Part Water Regenerate
+15 Pass Plant Speak With Monsters Reincarnate
+16 Plane Shift Stone Tell Restoration
+17 Quest Transmute Water to Dust Resurrection
+18 Rainbow Transport Via Plants Succor
+19 Raise Dead Turn Wood Sunray
+20 Spike Stones Wall of Thorns Symbol
+21 Transmute Rock to Mud Weather Summoning Transmute Metal to Wood
+22 True Seeing Word of Recall Wind Walk
+
+23 Wall of Fire
