cfe4d114452cb8a14a2568e5812c6d99aacd70d7
nerd2ninja <23155951+nerd2ninja@users.noreply.github.com>
2024-09-23 18:38:49 -0400

    Create dragonlance_gods.rb

---
+++ dragonlance_gods.rb
@@ -0,0 +1,5 @@
+#require "spells.rb"
+#A clerics god determines what spells they get based on that god's sphere of influence. The following is each god's sphere of influence
+paladine_spheres = [astral, charm, guardianship, protection, stellar]
+
+paladine_specials = [detect_balance, reflecting_pool, ]
