8bbc2055cf68227ce380a0aace33b5bff9e2445a
nerd2ninja <23155951+nerd2ninja@users.noreply.github.com>
2024-09-23 18:40:17 -0400

    Create TODO.rb

---
+++ TODO.rb
@@ -0,0 +1,37 @@
+=begin
+TODO List
+
+Add HP based on class
+Automatically increase scores after level up
+
+THAC0 and AC???? pg 122
+THAC0 is at each level for each class group. 
+Naked AC is 10
+Armor replaces AC entirely
+Dex modifiers either adds or subtracts to the AC.
+Lower AC is good
+Negative AC is amazing
+
+
+Make a Starting gold roll based on class
+(3d6) for Priests
+Make a starting equipment shop pg 90 Table 44
+
+Automatically add spells for clerics based on their diety's domains and the cleric's level
+pg 35 cleric
+pg 85 Priest Spells
+pg 129 Priest Spell list
+pg 199 Priest Spell descriptions
+pg 241 Priest Spells by Sphere
+
+Have a add spells feature to add spells by name for wizards depending on their level and specialty. Automatically add if relevent
+pg 81 magic
+pg 127 Wizard Spell list
+
+Export all data to Foundry .json file.
+Export all data to character sheet .pdf
+
+lnurl-auth logins
+Save all data to repl protected by login
+
+=end
